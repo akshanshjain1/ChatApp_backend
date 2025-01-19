d4bf7c1d8b45758c2760632cfecbe81b8e7686b7
Akshansh <akshanshj17@gmail.com>
2025-01-20 03:49:10 +0530

    updated commit

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,11 +1,7 @@
 import { CorsOptions } from "cors"
 
 const corsoption:CorsOptions={
-    origin:[
-        "http://localhost:5173",
-        "http://localhost:4173",
-        process.env.CLIENT_URL||""
-    ],
+    origin:"*",
     credentials:true
 }
 export {corsoption}
